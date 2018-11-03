7dfd694d723014894d5e16872cbea9174cd6ae72
mk2 <mk2@mk2.red>
2018-11-03 20:02:10 +0900

    update yawf-cli template.

--- packages/yawf-cli/templates/config/routes.js
+++ packages/yawf-cli/templates/config/routes.js
@@ -1,3 +1,3 @@
 export default {
-  'GET /': 'top.index'
+  'GET /': 'top'
 }
--- packages/yawf-cli/templates/server/actions/top.js
+++ packages/yawf-cli/templates/server/actions/top.js
@@ -1,5 +1,3 @@
-export default class {
-  index(req, res) {
-    res.render('top')
-  }
+export default function(req, res) {
+  res.render('top')
 }
