992d96745d7a55d6195ff3c2671e5738e96622fc
mk2 <mk2@mk2.red>
2018-12-06 18:35:17 +0900

    Use NODE_OPTIONS.

--- packages/yawf-core/__tests__/task/task.spec.js
+++ packages/yawf-core/__tests__/task/task.spec.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const Core = require('../../lib/core')
 
-process.argv.push('--experimental-worker')
-
 describe('Task specs', () => {
 
   test('Run task normally.', async () => {
