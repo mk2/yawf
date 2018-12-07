c8821ae8e93fb5a7dffceab8a64b87a213566cf3
mk2 <mk2@mk2.red>
2018-12-07 22:32:33 +0900

    Modify pre-commit hook script.

--- scripts/hook/pre-commit.js
+++ scripts/hook/pre-commit.js
@@ -1,9 +1,26 @@
+/**
+ * pre-commit script
+ * - check `npm run build` finishing normally.`
+ */
+
 const spawn = require('cross-spawn')
+const { chunksToLinesAsync } = require('@rauschma/stringio')
 
-const result = spawn.sync('npm', [ 'run', 'build' ], { stdio: 'inherit' })
-
-if (result.status === 0) {
-  process.exit(0)
-} else {
-  process.exit(1)
+async function echoReadable(readable) {
+  let contents = ''
+  for await (const line of chunksToLinesAsync(readable)) {
+    contents += line
+  }
+  return contents
 }
+
+async function main() {
+  const child = spawn('npm', [ 'run', 'build' ], { stdio: [ 'ignore', 'ignore', 'pipe' ] })
+  const contents = await echoReadable(child.stderr)
+  if (contents.indexOf('lerna ERR!') >= 0) {
+    process.exit(1)
+  } else {
+    process.exit(0)
+  }
+}
+main()
