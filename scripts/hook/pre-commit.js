f4cb3dc861e8a2e321ed96e04a4e20b8cad9c4cc
mk2 <mk2@mk2.red>
2018-12-07 21:07:52 +0900

    Modify pre-commit script.

--- scripts/hook/pre-commit.js
+++ scripts/hook/pre-commit.js
@@ -1,1 +1,9 @@
-process.exit(0)
+const spawn = require('cross-spawn')
+
+const result = spawn.sync('npm', [ 'run', 'build' ], { stdio: 'inherit' })
+
+if (!result.error) {
+  process.exit(0)
+} else {
+  process.exit(1)
+}
